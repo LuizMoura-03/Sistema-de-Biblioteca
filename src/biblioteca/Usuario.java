4fac7b234aeb8852eb81df3d9a1e5bf68c0aa799
LuizFernando <barbosa.luiz@zup.com.br>
2024-12-23 15:38:48 -0300

    :sparkles: feat: Adding display detail method to User class

--- src/biblioteca/Usuario.java
+++ src/biblioteca/Usuario.java
@@ -1,4 +1,25 @@
 package biblioteca;
 
 public class Usuario {
+    private String nome;
+    private int id;
+
+    public Usuario(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void exibirdetalhes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("ID: " + id)
+        ;
+    }
 }
