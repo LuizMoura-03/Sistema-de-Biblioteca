f3af05cb49a3a1c68032fe69bdba027ea1185b4a
LuizFernando <barbosa.luiz@zup.com.br>
2024-12-23 15:52:51 -0300

    :sparkles: feat: Adding methods to the Library class

--- src/biblioteca/Biblioteca.java
+++ src/biblioteca/Biblioteca.java
@@ -1,4 +1,31 @@
 package biblioteca;
 
+import java.util.ArrayList;
+
 public class Biblioteca {
+    private ArrayList<Livro> livros;
+    private ArrayList<Usuario> usuarios;
+
+    public Biblioteca() {
+        livros = new ArrayList<>();
+        usuarios = new ArrayList<>();
+    }
+
+    public void cadastrarLivro(Livro livro) {
+        livros.add(livro);
+        System.out.println("Livro cadastrado com sucesso!");
+    }
+
+    public void cadastrarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+        System.out.println("Usario cadastrado com sucesso!");
+    }
+
+    public void listarLivros() {
+        System.out.println("Lista de Livros");
+        for (Livro livro : livros) {
+            livro.exibirDetalhes();
+        }
+    }
+
 }
