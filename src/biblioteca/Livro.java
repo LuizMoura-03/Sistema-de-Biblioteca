0c759e7636e8560ce9cf943cca1f96d358b29e5c
LuizFernando <barbosa.luiz@zup.com.br>
2024-12-23 15:23:55 -0300

    :sparkles: feat: Adding methods to the book class

--- src/biblioteca/Livro.java
+++ src/biblioteca/Livro.java
@@ -1,4 +1,50 @@
 package biblioteca;
 
+import javax.sql.rowset.serial.SerialStruct;
+
 public class Livro {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private boolean disponivel;
+
+    public Livro(String titulo, String autor, String isbn, boolean disponivel) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.disponivel = disponivel;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void emprestar() {
+        if (disponivel) {
+            disponivel = false;
+            System.out.println("Livro emprestado com sucesso!");
+        } else {
+            System.out.println("O livro já esta emprestado.");
+        }
+    }
+
+    public void devolver() {
+        disponivel = true;
+        System.out.println("Livro devolvido com sucesso");
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Titulo: " + titulo) ;
+        System.out.println("Autor: " + autor);
+        System.out.println("ISBN: " + isbn);
+        System.out.println("Disponivel: " + (disponivel ? "Sim" : "Não"));
+    }
 }
